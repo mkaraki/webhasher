90d781f46150ca2d46211e8e71301f901b483976
mkaraki <26180919+mkaraki@users.noreply.github.com>
2025-03-12 05:00:40 +0900

    Add minimal hashing configuration

--- src/lib.rs
+++ src/lib.rs
@@ -17,13 +17,16 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub struct HashNameAndHashMap(HashMap<String, String>);
 
 #[wasm_bindgen]
-pub fn calc_hash(data: Vec<u8>) -> HashNameAndHashMap {
+pub fn calc_hash(data: Vec<u8>, hash_list: Vec<String>) -> HashNameAndHashMap {
     let hashers = all_hashers();
     let data = data.as_slice();
     let mut results: HashMap<String, String> = HashMap::new();
 
     for hasher in hashers {
         let hash_name = hasher.hash_name().parse().unwrap();
+        if !hash_list.contains(&hash_name) {
+            continue;
+        }
         let res = hasher.hash(data);
         let mut res_hex = String::new();
         for byte in res {
@@ -33,4 +36,17 @@ pub fn calc_hash(data: Vec<u8>) -> HashNameAndHashMap {
     }
 
     HashNameAndHashMap(results)
+}
+
+#[wasm_bindgen]
+pub fn list_hash_name() -> Vec<String> {
+    let hashers = all_hashers();
+    let mut results: Vec<String> = Vec::new();
+
+    for hasher in hashers {
+        let hash_name = hasher.hash_name().parse().unwrap();
+        results.push(hash_name);
+    }
+
+    results
 }
